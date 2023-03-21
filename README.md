2235fa3b706d765b0aac62323e8876f15b4767bc
baa30 <baa30@sfu.ca>
2023-03-21 10:00:43 -0700

    Init Repo
    Initial repository commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Google-ISR-Competition
+
+Files for the Google - Isolated Sign Language Recognition competition.
---
+++ data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Simplified bash script to update data via the kaggle API
+kaggle datasets version -p ./gislr_data -m "Updated data"
